4edad5f6874e88e10fb8e3fb5762be59665a99a7
elpir <ripleysinclair@proton.me>
2025-10-20 16:03:42 +0000

    Just studying Fourier Transformations a bit.

---
+++ src/main/java/org/riproot/fourier_analysis/FourierTransformation.java
@@ -0,0 +1,17 @@
+package org.riproot.fourier_analysis;
+
+// https://www.nayuki.io/page/how-to-implement-the-discrete-fourier-transform
+
+public class FourierTransformation {
+    /*
+     * The interpretation of the DFT is that the vector x represents the signal level at various points in time.
+     * The vector X represents the signal level at various frequencies.
+     * The formula outlines that the signal level at frequency k is equal to the sum of the 
+     * signal level at each time t multiplied by a complex exponential.
+     * 
+     * 
+     * The output of a Fourier Transformation is called a frequency spectrum, which contains a series of complex numbers where each number 
+     * corresponds to a specific frequnecy and provides information about it's amplitude. (How much of that frequency is present) and phase 
+     * (The offset of the frequency's waveform). Each output is in the form a + jb.    
+     */
+}
