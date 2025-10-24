a36a74b8b111eb3e0239ff7d0ca81c950edcdc7b
elpir <ripleysinclair@proton.me>
2025-10-24 15:48:54 +0000

    Finished the Fourier Transformation constructor, and added get() functions for Real numbers and Imaginary numbers.

--- src/main/java/org/riproot/fourier_analysis/FourierTransformation.java
+++ src/main/java/org/riproot/fourier_analysis/FourierTransformation.java
@@ -13,5 +13,62 @@ public class FourierTransformation {
      * The output of a Fourier Transformation is called a frequency spectrum, which contains a series of complex numbers where each number 
      * corresponds to a specific frequnecy and provides information about it's amplitude. (How much of that frequency is present) and phase 
      * (The offset of the frequency's waveform). Each output is in the form a + jb.    
+     * 
+     * After implementing it myself, I will move to the Apache Commons Math FFT. 
+     * 
+     * This will be part of a larger project called ASR. 
+     * 
+     * Pretty much I will:
+     * 
+     * Preprocess the audio (remove noise, normalize it)
+     * 
+     * Frame-Blocking (divide audio into short, overlapping timeframes)
+     * 
+     * Compute a FFT
+     * 
+     * Extract Features
+     * 
+     * Use a language model to convert the sequence into phonemes into recognizable words and sentances. 
      */
+
+     private double[] outReal;
+     private double[] outImg; 
+
+     public FourierTransformation(double[] inReal, double[] inImg){
+        // Assuming that inReal and inImg have the same length.
+        int n = inReal.length;
+        double[] outR = new double[n];
+        double[] outI = new double[n];
+
+        for(int k = 0; k < n; k++){ // for each output k. (This is the Summation)
+            double sumReal = 0;
+            double sumImg = 0;
+            for(int t = 0; t < n; t++){ // for each input element t. 
+                /*  Euler's formula tells us that e^xi = cos x + i sin x for any real number x. 
+                    cos -x = cos x, and sin -x = sin x.
+                    So,
+                    for a signal x(t), x(t)e^pi-2tk/n) = [Re(x(t)) + Im(x(t))][cos(pi2(tk/n)) - isin(pi2(tk/n))]
+                */
+                double angle = 2 * Math.PI * t * k / n; 
+                sumReal = inReal[t] * Math.cos(angle) + inImg[t] * Math.sin(angle);
+                sumImg = -inReal[t] * Math.sin(angle) + inImg[t] * Math.cos(angle); 
+
+
+            }
+            
+            outR[k] = sumReal;
+            outI[k] = sumImg;
+        }
+
+        this.outReal = outR;
+        this.outImg = outI; 
+     }
+
+     public double[] getReal(){
+        return outReal;
+     }
+     
+     public double[] getImg(){
+        return outImg;
+     }
 }
